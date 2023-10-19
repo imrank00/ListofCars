edbe8be2c939100acd99fc660aa43a564f421c48
imrank00 <113467979+imrank00@users.noreply.github.com>
2023-10-19 14:03:08 +0530

    Listofcar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import ListofCars from "./ListofCars";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <ListofCars />
+    </div>
+  );
+}
+
+export default App;
